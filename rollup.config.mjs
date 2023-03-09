9672184622bd2814278061e5d751bbb5da634320
Colin Dunn <colin@squidcloud.io>
2023-03-09 11:30:01 -0500

    initial commit

---
+++ rollup.config.mjs
@@ -0,0 +1,46 @@
+import del from 'rollup-plugin-delete';
+import typescript from 'rollup-plugin-typescript2';
+import external from 'rollup-plugin-peer-deps-external';
+import polyfills from 'rollup-plugin-node-polyfills';
+
+import terser from '@rollup/plugin-terser';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from "@rollup/plugin-commonjs"
+import json from "@rollup/plugin-json";
+
+import pkg from './package.json' assert {type: 'json'};
+
+const input = 'src/index.ts';
+const plugins = [
+  del({targets: 'dist/*', runOnce: true}),
+  typescript(),
+  external(),
+  resolve(),
+  commonjs(),
+  polyfills(),
+  json(),
+  terser(),
+];
+
+export default [
+  {
+    external: [
+      '@squidcloud/client',
+      '@squidcloud/common'
+    ],
+    input,
+    output: [
+      {
+        file: pkg.main,
+        format: 'cjs',
+        sourcemap: true,
+      },
+      {
+        file: pkg.module,
+        format: 'esm',
+        sourcemap: true,
+      },
+    ],
+    plugins,
+  }
+];
