eff104030d98932eeff8c1a3d6bb06c6c98df38f
Colin Dunn <colin@squidcloud.io>
2023-10-17 15:09:52 -0700

    initial commit

--- src/hooks/useQuery.ts
+++ src/hooks/useQuery.ts
@@ -19,12 +19,14 @@ type GetReturnType<T> = T extends SnapshotEmitter<infer U> ? U : never;
  * @param query The query object.
  * @param subscribe If true, subscribe to query snapshots. Default is false.
  * @param initialValue The initial value of the data.
+ * @param deps Array of dependencies for the hook. Default is [].
  * @returns The query data, loading state, and error.
  */
 export function useQuery<T extends DocumentData>(
   query: T & SnapshotEmitter<any>,
   subscribe = false,
   initialValue?: Array<GetReturnType<T>>,
+  deps: ReadonlyArray<unknown> = [],
 ): QueryType<GetReturnType<T>> {
   const peekInitialValue = () => {
     try {
@@ -36,7 +38,7 @@ export function useQuery<T extends DocumentData>(
   const { loading, error, data } = useObservable<GetReturnType<T>[]>(
     () => (subscribe ? query.snapshots() : from(query.snapshot())),
     initialValue || peekInitialValue(),
-    [JSON.stringify(query.serialize()), subscribe],
+    [JSON.stringify(query.serialize()), subscribe, JSON.stringify(deps)],
   );
   return { loading, error, data };
 }
