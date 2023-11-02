3a750b6d9a6b8e49d7bf96c4ac66920daea07c62
Nir Peled <peledni@gmail.com>
2023-11-02 17:29:53 -0400

    Added ai on your data hook

--- src/hooks/useAiAssistant.ts
+++ src/hooks/useAiAssistant.ts
@@ -2,9 +2,10 @@
 
 import {AiAssistantChatOptions, generateId, IntegrationId} from '@squidcloud/common';
 import {useEffect, useState} from 'react';
-import {of} from 'rxjs';
+import {from, map, of} from 'rxjs';
 import {useObservable} from './useObservable';
 import {useSquid} from './useSquid';
+import {assertTruthy} from 'assertic';
 
 export type ChatMessage = {
   id: string;
@@ -12,8 +13,26 @@ export type ChatMessage = {
   message: string;
 };
 
-export function useAiAssistant(integrationId: IntegrationId, profileId: string) {
+interface AiHookResponse {
+  chat: (prompt: string, options?: AiAssistantChatOptions) => void;
+  history: ChatMessage[];
+  data: string;
+  loading: boolean;
+  error: any;
+  complete: boolean;
+}
+
+export function useAiQuery(integrationId: IntegrationId): AiHookResponse {
+  return useAiHook(integrationId, true);
+}
+
+export function useAiAssistant(integrationId: IntegrationId, profileId: string): AiHookResponse {
+  return useAiHook(integrationId, false, profileId);
+}
+
+function useAiHook(integrationId: string, aiQuery: boolean, profileId?: string): AiHookResponse {
   const squid = useSquid();
+  assertTruthy(!aiQuery || squid.options.apiKey, 'apiKey must be defined for AI queries')
   const [question, setQuestion] = useState('');
   const [aiAssistantOptions, setAiAssistantOptions] = useState<AiAssistantChatOptions | undefined>(undefined);
   const [history, setHistory] = useState<Array<ChatMessage>>([]);
@@ -21,7 +40,12 @@ export function useAiAssistant(integrationId: IntegrationId, profileId: string) 
   const {data, error, loading, complete} = useObservable(
     () => {
       if (!question) return of('');
-      return squid.ai().assistant(integrationId).profile(profileId).chat(question, aiAssistantOptions);
+      if (aiQuery) {
+        return from(squid.ai().executeAiQuery(integrationId, question)).pipe(map((response) => response.answer));
+      } else {
+        assertTruthy(profileId, 'profileId must be defined');
+        return squid.ai().assistant(integrationId).profile(profileId).chat(question, aiAssistantOptions);
+      }
     },
     '',
     [question],
