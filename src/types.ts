d2995627e77e19c993f7301e11959e3e71655e7c
Mikhail Fursov <mike.fursov@gmail.com>
2024-08-05 14:10:37 -0500

    cleanup

--- src/types.ts
+++ src/types.ts
@@ -1,8 +1,6 @@
-import { SnapshotEmitter, Squid } from '@squidcloud/client';
+import { Squid } from '@squidcloud/client';
 
 /** Type representing the context for the Squid library. */
 export type SquidContextType = {
   squid: Squid | null;
 };
-
-export type SnapshotEmitterReturnType<T> = T extends SnapshotEmitter<infer U> ? U : never;
